b268fd68ab4f1a7ffece64092c8abde851e84d5b
wipeautcrafter <wipeautcrafter@gmail.com>
2021-10-22 22:06:32 +0200

    first working version

--- main.py
+++ main.py
@@ -3,16 +3,29 @@ import paho.mqtt.client as mqtt
 
 from time import sleep
 from sys import stdout
+from math import floor
 import numpy as np
-import json
+import requests
 
 class Display:
     def __init__(self):
         # assign pil image
-        self.font = './font/NotoSansMono-Regular.ttf'
-        self.state = None
-        self.scroll = 0
-        
+        self.font = './font/TerminusTTF-4.49.1.ttf'
+        self.big_font = ImageFont.truetype(self.font, size=16)
+        self.small_font = ImageFont.truetype(self.font, size=12)
+
+        self.state = {
+            'title': 'Je Mama',
+            'artist': '- Wiebe',
+            'status': 'play',
+            'seek': (69 * 60) * 1000,
+            'duration': (69 * 60) * 2,
+            'volume': 100
+        }
+
+        self.t_scroll = 0
+        self.a_scroll = 0
+
         # subscribe to player mqtt
         self.client = mqtt.Client()
         self.client.on_message = self.mqtt_handle
@@ -20,41 +33,68 @@ class Display:
     def start(self):
         self.client.connect('mqtt.bitlair.nl')
         self.client.loop_start()
-        self.client.subscribe('djo/player/state')
+
+        self.client.subscribe('djo/player/#')
 
         while True:
-            if self.state is not None:
-                self.frame()
-            
-            sleep(0.25)
+            self.frame()
 
     def mqtt_handle(self, _c, _u, msg):
-        state = msg.payload.decode('utf-8')
-        self.state = json.loads(state)
-
-        self.frame()
+        self.state[msg.topic.split('/')[-1]] = msg.payload.decode()
 
     def frame(self):
         image = Image.new('RGB', (120, 48), 'black')
 
         draw = ImageDraw.Draw(image)
-        font = ImageFont.truetype(self.font, size=16)
+        font = ImageFont.truetype(self.font, size=15)
 
-        txt = self.state['title'] + ' | '
-        
-        self.scroll += 1
-        if self.scroll >= len(txt):
-            self.scroll = 0
+        title = self.state['title']
+        artist = self.state['artist']
 
-        txt = txt[self.scroll:] + txt[:self.scroll]
-        
-        draw.text((60, 24), txt, fill='green', anchor='mm', font=font)
+        t_width = font.getsize(title)[0]
+        a_width = font.getsize(artist)[0]
+
+        p_text = '- paused -'
+        if self.state['status'] == 'play':
+            seconds = int(int(self.state['seek']) / 1000)
+            minutes = floor(seconds / 60)
+            seconds -= minutes * 60
+
+            p_text = str(minutes)
+            p_text += ':' + ('0' if seconds < 10 else '')
+            p_text += str(seconds)
+
+        draw.text((60, 46), p_text, fill='green', anchor='mb', font=self.small_font)
+
+        progress = (int(self.state['seek']) / 1000) / int(self.state['duration'])
+        draw.line([(0, 46), (int(119 * progress), 46)], fill='green', width=2)
+
+        progress = int(self.state['volume']) / 100
+        draw.line([(0, 0), (int(119 * progress), 0)], fill='red', width=1)
+
+        if t_width >= 120:
+            self.t_scroll += 2.5
+            if self.t_scroll >= t_width + 150:
+                self.t_scroll = 0
+            
+            draw.text((120 - self.t_scroll, 8), title, fill='orange', anchor='lm', font=self.big_font)
+        else:
+            draw.text((60, 8), title, fill='orange', anchor='mm', font=self.big_font)
+
+        if a_width >= 120:
+            self.a_scroll += 2.5
+            if self.a_scroll >= a_width + 150:
+                self.a_scroll = 0
+            
+            draw.text((120 - self.a_scroll, 22), artist, fill='orange', anchor='lm', font=self.big_font)
+        else:
+            draw.text((60, 22), artist, fill='orange', anchor='mm', font=self.big_font)
 
         self.output(image)
     
     def output(self, image):
         # [[r, g, b], [r, g, b]] -> [r, g, b, r, g, b]
-        data = np.asarray(image).flatten()
+        data = np.asarray(image, dtype='uint8').flatten()
         # [r, g, b, r, g, b] -> [r, g, r, g]
         data = np.delete(data, np.arange(2, data.size, 3))
 
--- visualize.py
+++ visualize.py
@@ -11,19 +11,24 @@ class Visualize:
     def start(self):
         while True:
             read = stdin.buffer.read(3)
-
-            if read == b':00':
+            
+            if read.startswith(b':00'):
                 read = stdin.buffer.read(self.bufsize)
 
-                if len(read) >= self.bufsize and read.endswith(b'\x00'):
-                    self.read(read[:-1])
+                if len(read) >= self.bufsize:
+                    if read.startswith(b':00'):
+                        read = read[3:]
+                    if read.endswith(b'\x00'):
+                        read = read[:-1]
+                    
+                    self.read(read)
 
     def read(self, read):
         # b':00' + [... bytes ...] + b'\x00'
         barr = bytearray(read)
 
         # [... bytes ...] -> '... 11001010101 ...'
-        bits = "".join(["{0:b}".format(l) for l in barr])
+        bits = ["{0:b}".format(l) for l in barr]
         bits = [bits[i:i+2] for i in range(0, len(bits), 2)]
         arr = [[int(l[0]) * 255, int(l[1]) * 255, 0] for l in bits]
         arr = np.asarray(arr, dtype='uint8').reshape((48, 120, 3))
